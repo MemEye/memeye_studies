64fc80a2e1cd9658989fe874621874de2dc13e88
monaabd27 <monaabd999@gmail.com>
2023-12-11 10:08:56 -0500

    Update time_align_annotations.py

---
+++ data_analysis/time_align_annotations.py
@@ -0,0 +1,11 @@
+import numpy as np
+import csv
+import pandas as pd
+
+
+
+
+# load in the annotations CSV from pupil (and any other annotation files), 
+# and the data CSV from the emotibit
+
+# expand the annotations CSV to have all of the time stamps for the 
