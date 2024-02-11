b200593c58f1aae683e0c16663710c68166b0504
Saksham0109 <f20212414@pilani.bits-pilani.ac.in>
2024-02-11 17:23:41 +0530

    .

--- dataloader.py
+++ dataloader.py
@@ -3,10 +3,11 @@ from dataset import Dataset
 from torch.utils.data import DataLoader
 
 def get_dataloader(sec=2, batch_size=64):
+    input_dim=10
     seq_length=int(sec*25)
-    train_dataset = torch.load('train4.pt')
-    val_dataset = torch.load('val4.pt')
-    test_dataset = torch.load('test4.pt')
+    train_dataset = torch.load('train.pt')
+    val_dataset = torch.load('val.pt')
+    test_dataset = torch.load('test.pt')
     #Make a dataloader by breaking into sequence of 64 length
     train_dataset_new=[]
     count=0
@@ -15,29 +16,36 @@ def get_dataloader(sec=2, batch_size=64):
         for i in range(0,len(x[0]),seq_length):
             label=torch.argmax(x[1])
             no_of_labels[label]+=1
+    count2=no_of_labels[1]-no_of_labels[2]
+    print(no_of_labels)
+    count3=no_of_labels[1]-no_of_labels[3]
     for x in train_dataset:
         if len(x[0])%seq_length!=0:
-            x[0]=torch.cat((x[0],torch.zeros(seq_length-len(x[0])%seq_length,10)))
+            x[0]=torch.cat((x[0],torch.zeros(seq_length-len(x[0])%seq_length,input_dim)))
         for i in range(0,len(x[0]),seq_length):
             label=torch.argmax(x[1])
             if label==0:
                 count+=1
-            if label==0 and count>=max(no_of_labels[2],max(no_of_labels[3]*2,no_of_labels[1]*2)):
+            if label==0 and count>=no_of_labels[1]:
                 continue
-            if label==1 or label==3:
+            if label==2 and count2>=0:
                 train_dataset_new.append([x[0][i:i+seq_length],x[1]])
+                count2-=1
+            if label==3 and count3>=0:
+                train_dataset_new.append([x[0][i:i+seq_length],x[1]])
+                count3-=1
             train_dataset_new.append([x[0][i:i+seq_length],x[1]])
 
     val_dataset_new=[]
     for x in val_dataset:
         if len(x[0])%seq_length!=0:
-            x[0]=torch.cat((x[0],torch.zeros(seq_length-len(x[0])%seq_length,10)))
+            x[0]=torch.cat((x[0],torch.zeros(seq_length-len(x[0])%seq_length,input_dim)))
         for i in range(0,len(x[0]),seq_length):
             val_dataset_new.append([x[0][i:i+seq_length],x[1]])
     test_dataset_new=[]
     for x in test_dataset:
         if len(x[0])%seq_length!=0:
-            x[0]=torch.cat((x[0],torch.zeros(seq_length-len(x[0])%seq_length,10)))
+            x[0]=torch.cat((x[0],torch.zeros(seq_length-len(x[0])%seq_length,input_dim)))
         for i in range(0,len(x[0]),seq_length):
             test_dataset_new.append([x[0][i:i+seq_length],x[1]])
     print(len(train_dataset_new),len(val_dataset_new),len(test_dataset_new))
@@ -47,7 +55,7 @@ def get_dataloader(sec=2, batch_size=64):
     return train_dataloader,val_dataloader,test_dataloader
 
 if __name__ == "__main__":
-    for sec in [2,2.25,2.5,2.75,3,3.25,3.5]:
+    for sec in [3]:
         train_dataloader,val_dataloader,test_dataloader = get_dataloader(sec=sec)
         torch.save(train_dataloader,'train'+str(sec)+'_dataloader.pt')
         torch.save(val_dataloader,'val'+str(sec)+'_dataloader.pt')
--- dataset.py
+++ dataset.py
@@ -6,16 +6,54 @@ import glob
 class Dataset(torch.utils.data.Dataset):
     def __init__(self,start,end):
         self.data=[]
-        for x in ['emotibit_segmented/*']:
+        for x in ['emotibit_segmented_new/*']:
             for file in glob.glob(x):
                 if int(file[-3:])>=start and int(file[-3:])<=end:
                     for f in glob.glob(file+'/*'):
-                        label=f.split('/')[-1]
-                        if label=='recognition_new':
-                            continue
-                        if label in ['negative','learning','recall','recognition_familar']:
+                        l=f.split('/')[-1]
+                        if l in ['negative','learning','recall','recognition_familar']:
                             for csvs in glob.glob(f+'/*.csv'):
+                                label=l
                                 df = pd.read_csv(csvs)
+                                # x=df['remembered'].value_counts()
+                                # if label=='recall':
+                                #     if 'Yes' in x:
+                                #         if 'No' in x:
+                                #             if x['Yes']<x['No']:
+                                #                 label='recall_fail'
+                                #             else:
+                                #                 label='recall_correct'
+                                #         else:
+                                #             label='recall_correct'
+                                #     else:
+                                #         label='recall_fail'
+                                # elif label=='recognition_familar':
+                                #     if 'Yes' in x:
+                                #         if 'No' in x:
+                                #             if x['Yes']<x['No']:
+                                #                 label='recognition_fail'
+                                #             else:
+                                #                 label='recognition_correct'
+                                #         else:
+                                #             label='recognition_correct'
+                                #     else:
+                                #         label='recognition_fail'
+                                # elif label=='recognition_new':
+                                #     if 'No' in x:
+                                #         if 'Yes' in x:
+                                #             if x['No']>x['Yes']:
+                                #                 label='recognition_fail'
+                                #             else:
+                                #                 continue
+                                #         else:
+                                #             continue
+                                if df['is_question'].isna().all() and df['is_verbal'].isna().all():
+                                    pass
+                                else:
+                                    for index,row in df.iterrows():
+                                        if not pd.isna(row['is_question']) or not pd.isna(row['is_verbal']):
+                                            break
+                                    df=df[:index]
                                 df=df[['T1','TH','EA','EL','PI','PR','PG','SF','SR','SA']]
                                 for col in df.columns:
                                     df[col]=(df[col]-df[col].mean())/df[col].std()
@@ -23,6 +61,7 @@ class Dataset(torch.utils.data.Dataset):
                                 df=df.values
                                 df=torch.tensor(df,dtype=torch.float32)
                                 #Create one hot encoding for labels
+
                                 if label=='negative':
                                     label=torch.tensor([1,0,0,0],dtype=torch.float32)
                                 elif label=='learning':
@@ -31,6 +70,8 @@ class Dataset(torch.utils.data.Dataset):
                                     label=torch.tensor([0,0,1,0],dtype=torch.float32)
                                 elif label=='recognition_familar':
                                     label=torch.tensor([0,0,0,1],dtype=torch.float32)
+                                else:
+                                    continue
                                 self.data.append([df,label])
     def __len__(self):
         return len(self.data)
@@ -40,9 +81,9 @@ class Dataset(torch.utils.data.Dataset):
     
 #Save the dataset in train.pt
 train_dataset=Dataset(101,120)
-torch.save(train_dataset,'train4.pt')
+torch.save(train_dataset,'train.pt')
 val_dataset=Dataset(121,123)
-torch.save(val_dataset,'val4.pt')
+torch.save(val_dataset,'val.pt')
 test_dataset=Dataset(124,132)
-torch.save(test_dataset,'test4.pt')
+torch.save(test_dataset,'test.pt')
 
